e492a3c10e2cca56239c84c0098b580ba4f7b833
zcat07 <143707195+ganesh9714@users.noreply.github.com>
2025-05-11 04:55:05 +0530

    smoll-changes

--- job-portal-client/src/Components/NewsLetter.jsx
+++ job-portal-client/src/Components/NewsLetter.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import { FaEnvelopeOpenText, FaRocket } from "react-icons/fa";
 
 const NewsLetter = () => {
@@ -9,7 +9,7 @@ const NewsLetter = () => {
                 <FaEnvelopeOpenText />Email me for Jobs
             </h3>
             <p className='text-primary/75 text-base mb-4'>
-                We are excited to inform you what's really happening. Subscribe to our newsletter today.
+                We are excited to inform you what&apos;s really happening. Subscribe to our newsletter today.
             </p>
 
             <div className='w-full space-y-4'>
